64b1a8dcab6502ecf0be982aff88b1fa24cd471b
Nina Paljakka-Cargo <ninamarica17@gmail.com>
2021-04-17 11:16:20 -0500

    dependencies

---
+++ app.py
@@ -0,0 +1,19 @@
+#import dependencies
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+#Use Flask to create your routes.
+
+
+/api/v1.0/precipitation
+
+
+Convert the query results to a dictionary using date as the key and prcp as the value.
+
+
+Return the JSON representation of your dictionary.
