830b9bbbac02ddabb3f0a9a84a3b7d8a39780f27
Nina Paljakka-Cargo <ninamarica17@gmail.com>
2021-04-17 12:15:29 -0500

    working on precipitation data

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 #import dependencies
-from flask import Flask
+from matplotlib import style
+style.use('fivethirtyeight')
+import matplotlib.pyplot as plt
+from flask import Flask, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+engine = create_engine("sqlite:///hawaii2.sqlite")
+base = automap_base()
+base.prepare(engine,reflect=True)
+measurement_class = base.classes.measurement
+station_class = base.classes.station
+session = Session(engine)
+
 app = Flask(__name__)
 
 #Use Flask to create your routes.
@@ -22,6 +37,10 @@ def route_1():
         f"<br/>"
         f"/api/v1.0/<start>/<end><br/>"
         f"-->Temperature data for dates between the start and end dates<br/>"
-     )
+    )
 
+@app.route("/api/v1.0/precipitation")
+def precipitation_data():
+    recent_date = session.query(func.max(measurement_class.date)).first()
+    last_12months = dt.date(2017,8,23)-dt.timedelta(days=365)
 
